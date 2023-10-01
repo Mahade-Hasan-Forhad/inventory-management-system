83cefe57414d1f2af87d957184d449f6b8bc0f85
Mahade Hasan Forhad <mahadehasanforhad@gmail.com>
2023-10-01 23:13:16 +0600

    Initialize project

---
+++ src/test/java/com/forhad/ims/InventoryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forhad.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
